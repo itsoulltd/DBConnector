154b5cf58ad6487469224f744896a00f4f0bddfa
Towhid <m.towhid.islam@gmail.com>
2018-06-14 14:43:25 +0600

    convanients added

--- src/com/it/soul/lab/sql/query/models/Table.java
+++ src/com/it/soul/lab/sql/query/models/Table.java
@@ -8,13 +8,10 @@ public class Table {
 	public Table() {
 		super();
 	}
-
 	private List<Row> rows = new ArrayList<Row>();
-
 	public List<Row> getRows() {
 		return rows;
 	}
-
 	public void setRows(List<Row> items) {
 		if (items == null) {return;}
 		this.rows = items;
@@ -23,5 +20,13 @@ public class Table {
 		if (list == null || rows.contains(list) == true){return this;}
 		rows.add(list);
 		return this;
+	}
+	public <T> List<T> inflate(Class<T> type) throws InstantiationException, IllegalAccessException{
+		List<T> inflatedRows = new ArrayList<>();
+		for (Row row : getRows()) {
+			T item = (T) row.inflate(type);
+			inflatedRows.add(item);
+		}
+		return inflatedRows;
 	}
 }
