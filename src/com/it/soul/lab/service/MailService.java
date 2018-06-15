d147cbca227ba21494779fe631a20be2c4995d48
Towhid <m.towhid.islam@gmail.com>
2018-06-15 21:58:48 +0600

    Email support tested for SSL ... okey

--- src/com/it/soul/lab/service/MailService.java
+++ src/com/it/soul/lab/service/MailService.java
@@ -1,71 +1,185 @@
 package com.it.soul.lab.service;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.Serializable;
 import java.util.Properties;
 
 import javax.mail.Authenticator;
+import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMessage.RecipientType;
 
 public class MailService implements Serializable{
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 7405779117813575373L;
 	
-	public synchronized void sendEmail(GMailAuthenticator auth, String subject, String message, String[] receivers){
+	public enum ContentType{
+		HTML("text/html"),
+		PLAIN("text/plain");
 		
-		Properties props = new Properties();
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.socketFactory.port", "465");
-		props.put("mail.smtp.socketFactory.class",
-				"javax.net.ssl.SSLSocketFactory");
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.port", "465");
+		private String contentValue;
+		private ContentType(String str) {
+			contentValue = str;
+		}
 		
-		try {
-			Session session = Session.getDefaultInstance(props 
-					, auth);
-			MimeMessage msg = new MimeMessage(session);
-			msg.setFrom(new InternetAddress(auth.getUserEmail()));
-			
-			int c = 0;
-			for (String address : receivers) {
-				if(c++ == 0){
-					msg.addRecipients(RecipientType.TO, address);
-					continue;
-				}
-				msg.addRecipients(RecipientType.CC, address);
+		@Override
+		public String toString() {
+			return contentValue;
+		}
+	}
+	
+	public enum MailServiceType{
+		TLS,
+		SSL
+	}
+
+	private static final long serialVersionUID = 7405779117813575373L;
+	private String subject;
+	private String[] receivers;
+	private Authenticator auth;
+	private Properties props;
+	private MailServiceType type;
+	private Session session;
+	
+	public static class Builder {
+		private MailService temp;
+		
+		public Builder(MailServiceType type) {
+			temp = new MailService();
+			temp.type = type;
+			Properties props = new Properties();
+			if(type == MailServiceType.TLS) {
+				props.put("mail.smtp.auth", "true");
+				props.put("mail.smtp.starttls.enable", "true");
+				props.put("mail.smtp.host", "smtp.gmail.com");
+				props.put("mail.smtp.port", "587");
+				//props.put("mail.smtp.debug", "true");
+			}else {
+				props.put("mail.smtp.host", "smtp.gmail.com");
+				props.put("mail.smtp.socketFactory.port", "465");
+				props.put("mail.smtp.socketFactory.class",
+						"javax.net.ssl.SSLSocketFactory");
+				props.put("mail.smtp.auth", "true");
+				props.put("mail.smtp.port", "465");
+				//props.put("mail.smtp.debug", "true");
 			}
-			
-			msg.setSubject(subject);
-			msg.setText(message);
-			
-			Transport.send(msg);
-			
+			temp.setProps(props);
+		}
+		public MailService build() {
+			return temp;
+		}
+		public Builder createAuth(String email, String password) {
+			GMailAuthenticator auth = new GMailAuthenticator(email, password);
+			temp.setAuth(auth);
+			return this;
+		}
+		public Builder subject(String subject) {
+			temp.setSubject(subject);
+			return this;
+		}
+		public Builder receivers(String...vers) {
+			temp.setReceivers(vers);
+			return this;
+		}
+	}
+	
+	@SuppressWarnings("static-access")
+	public synchronized void sendMail(String message, ContentType contentType){
+		try {
+			MimeMessage msg = createMessage();
+			if(contentType == null) {
+				msg.setText(message);
+			}else {
+				msg.setContent(message, contentType.toString());
+			}
+			if(type == MailServiceType.TLS) {
+				Transport tps = session.getTransport("smtps");
+				tps.send(msg);
+				tps.close();
+			}else {
+				Transport.send(msg);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
+	@SuppressWarnings("static-access")
+	public synchronized void sendMail(File html){
+		try {
+			MimeMessage msg = createMessage();
+			BufferedReader br = new BufferedReader(new FileReader(html));
+			try {
+			    StringBuilder sb = new StringBuilder();
+			    String line;
+			    while ((line = br.readLine()) != null) {
+			        sb.append(line);
+			        sb.append(System.lineSeparator());
+			    }
+			    String text = sb.toString();
+			    msg.setContent(text, ContentType.HTML.toString());
+			} finally {
+			    br.close();
+			}
+			if(type == MailServiceType.TLS) {
+				Transport tps = session.getTransport("smtps");
+				tps.send(msg);
+				tps.close();
+			}else {
+				Transport.send(msg);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private MimeMessage createMessage() throws AddressException, MessagingException {
+		session = Session.getInstance(props, auth);
+		MimeMessage msg = new MimeMessage(session);
+		msg.setFrom(new InternetAddress(((GMailAuthenticator)auth).getUserEmail()));
+		int c = 0;
+		for (String address : receivers) {
+			if(c++ == 0){
+				msg.addRecipients(RecipientType.TO, address);
+				continue;
+			}
+			msg.addRecipients(RecipientType.CC, address);
+		}
+		msg.setSubject(subject);
+		return msg;
+	}
 
-	public static class GMailAuthenticator extends Authenticator {
-		
-		String userEmail;
-		String password;
-		public GMailAuthenticator (String email, String password)
-		{
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public void setReceivers(String[] receivers) {
+		this.receivers = receivers;
+	}
+
+	public void setAuth(GMailAuthenticator auth) {
+		this.auth = auth;
+	}
+
+	public void setProps(Properties props) {
+		this.props = props;
+	}
+
+	private static class GMailAuthenticator extends Authenticator {
+		private String userEmail;
+		private String password;
+		public GMailAuthenticator (String email, String password){
 			super();
 			this.userEmail = email;
 			this.password = password;
 		}
-		public PasswordAuthentication getPasswordAuthentication()
-		{
-			return new PasswordAuthentication(userEmail, password);
+		public PasswordAuthentication getPasswordAuthentication(){
+			return new PasswordAuthentication(getUserEmail(), getPassword());
 		}
 		public String getUserEmail() {
 			return userEmail;
@@ -73,7 +187,6 @@ public class MailService implements Serializable{
 		public String getPassword() {
 			return password;
 		}
-		
 	}
 
 
---
+++ src/com/it/soul/lab/unit/test/MailSerciceTest.java
@@ -0,0 +1,41 @@
+package com.it.soul.lab.unit.test;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.it.soul.lab.service.MailService;
+import com.it.soul.lab.service.MailService.ContentType;
+import com.it.soul.lab.service.MailService.MailServiceType;
+
+public class MailSerciceTest {
+	
+	private MailService service;
+	
+	@Before
+	public void before() {
+		service = new MailService.Builder(MailServiceType.SSL)
+								.createAuth("itracker26@gmail.com", "...")
+								.subject("IT SOUL LIMITED")
+								.receivers("m.towhid.islam@gmail.com").build();
+	}
+	
+	@After
+	public void after() {
+		service = null;
+	}
+
+	@Test
+	public void testSendMailString() {
+		service.sendMail("<h1>This is actual message</h1>", ContentType.HTML);
+		Assert.assertTrue(true);
+	}
+
+	@Test
+	public void testSendMailFile() {
+		service.sendMail("This is actual message", ContentType.PLAIN);
+		Assert.assertTrue(true);
+	}
+
+}
